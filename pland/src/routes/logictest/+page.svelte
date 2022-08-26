f0555e403f9d3218347d70b0867cf14fb81a4c72
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2022-08-26 18:05:58 -0400

    use same function for logic test as lobbies 😒

--- pland/src/routes/logictest/+page.svelte
+++ pland/src/routes/logictest/+page.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
-    import { items } from '$lib/data/json/alttpr-customizer-schema.json';
     import type World from "$lib/z3r/logic/World";
     import Inverted from "$lib/z3r/logic/World/Inverted";
     import Retro from "$lib/z3r/logic/World/Retro";
     import Standard from "$lib/z3r/logic/World/Standard";
-    import { ItemCollection } from '$lib/z3r/logic/Support/ItemCollection';
-    import { LocationCollection } from '$lib/z3r/logic/Support/LocationCollection';
     import Presets from '$lib/components/Presets.svelte'
     import Plant from '$lib/components/Plant.svelte';
     import Open from '$lib/z3r/logic/World/Open';
     import type { IItem } from '$lib/z3r/logic/Item';
     import type { Z3rLocation } from '$lib/z3r/logic/Location';
-    import Item from '$lib/z3r/logic/Item';
     import type { PageData} from './$types';
+    import { checkPlants } from '$lib/z3r/logic/Logic'
 
     let selectedPreset: string = '';
     let world: World;
@@ -63,94 +60,10 @@
         selectedItems = selectedItems;
     }
 
-    function checkPlants() {
-        world.messages = logicTestMessages = [];
-        try {
-            let plantable = true;
-            let available = new ItemCollection([
-                Item.get('RescueZelda',world)!,
-                Item.get('Crystal1',world)!,
-                Item.get('Crystal2',world)!,
-                Item.get('Crystal3',world)!,
-                Item.get('Crystal4',world)!,
-                Item.get('Crystal5',world)!,
-                Item.get('Crystal6',world)!,
-                Item.get('Crystal7',world)!,
-                Item.get('PendantOfWisdom',world)!,
-                Item.get('PendantOfCourage',world)!,
-                Item.get('PendantOfPower',world)!,
-                Item.get('DefeatAgahnim',world)!,
-                Item.get('DefeatAgahnim2',world)!,
-            ]);
-            available.setChecksForWorld(world);
-            let planted = new LocationCollection();
-
-            items.forEach(item => {
-                if (item.count && item.count > 0)
-                {
-                    let itemObj = Item.get(item.value, world);
-                    if (!itemObj)
-                        itemObj = Item.get(item.value.slice(0, -2), world);
-                    
-                    if (itemObj) {
-                        for(let i = 0; i < item.count; i++) {
-                            available.addItem(itemObj);
-                        }
-                    }
-                }
-            });
-
-            world.resetPlants();
-            
-            for(let i = 0; i < selectedItems.length; i++) {
-                let location = world.locations.get(selectedLocations[i]!.name)!;
-                let item = selectedItems[i]!;
-                logicTestMessages.push(`Attempting to plant ${item.name} in ${location.name}.`);
-                if (!available.has(item.name)) {
-                    logicTestMessages.push(`${item.name} not available to plant.`);
-                    plantable = false;
-                    break;
-                }
-                if (location.hasItem() && !location.hasItem(item)) {
-                    logicTestMessages.push(`${location.name} already has item planted: ${location.item?.name}.`);
-                    plantable = false;
-                    break;
-                }
-                available.removeItem(item);
-                
-                plantable = plantable && location.canFill(item, available, true, planted)!;
-                
-                if (!plantable) {
-                    logicTestMessages.push(`Could not plant ${item.name} in ${location.name}.`)
-                    break;
-                }
-                else {
-                    if (location.fill(item, available)) {
-                        logicTestMessages.push(`Planted ${item.name} at ${location.name}.`)
-                        planted.addItem(location);
-                    }
-                    else {
-                        logicTestMessages.push(`Unknown error occurred. Could not Plant ${item.name} in ${location.name}.`);
-                    }
-                }
-            }
-
-            if (plantable) {
-                planted.to_array().map(location => location as Z3rLocation).forEach(location => {
-                    if (location.region.canEnter(world.locations, available) && location.canAccess(available) && location.item) {
-                        available.addItem(location.item);
-                    }
-                });
-                plantable &&= world.canPlaceMedallions(available);
-                plantable &&= world.canPlaceBosses();
-                plantable &&= world.canPlacePrizes(available);
-            }
-
-            logicTestResult = plantable;
-        }
-        catch (err:any) {
-            logicTestMessages.push(err);
-        }
+    function checkPlantsClick() {
+        const {plantable, messages} = checkPlants(world, selectedItems, selectedLocations);
+        logicTestResult = plantable;
+        logicTestMessages = messages;
     }
 </script>
 
@@ -170,7 +83,7 @@
             <button on:click="{() => removePlant(index)}">Remove Plant</button>
         {/each}
         <br/><br/>
-        <button on:click="{checkPlants}">Check Plants</button>
+        <button on:click="{checkPlantsClick}">Check Plants</button>
         {#if logicTestResult}✅{:else if logicTestResult == null}☑️{:else}❌{/if}
         <br/>
         <ul>
