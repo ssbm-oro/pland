727df7a0438c7fd03abadc21eeb99b04228cbe43
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2022-08-27 20:45:37 -0400

    much better surface color

--- pland/src/lib/components/Z3rItemSelector.svelte
+++ pland/src/lib/components/Z3rItemSelector.svelte
@@ -32,7 +32,7 @@
 <main>
     <Card>
         {#if disabled || Item && Item.name != "Random"}
-            <Button variant="filled-primary" on:click={changeSelection} {disabled}>
+            <Button variant="ring-primary" on:click={changeSelection} {disabled}>
                 <svelte:fragment slot="lead">
                     <Icon icon={Item.icon?.icon} color={Item.icon?.color} hFlip={Item.icon?.hFlip} vFlip={Item.icon?.vFlip}></Icon>
                 </svelte:fragment>
